d3e9d8f3173fc4901ea7e63d0d2ac43c41eee1e6
cem <cemyilmaztech@gmail.com>
2024-12-10 23:39:37 +0300

    model

--- src/dbConnection.js
+++ src/dbConnection.js
@@ -5,3 +5,4 @@ const mongoose = require('mongoose')
 mongoose.connect(( process.env.MONGODB || 'mongodb://localhost:27017/'))
 .then(() => console.log(' * DB Connected * ') )
 .catch((err) => console.log(' * DB Not Connected *', err))
+
