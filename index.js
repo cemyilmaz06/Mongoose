c2aeb88fe22c4300093e0c308c015dfc85690e47
cem <cemyilmaztech@gmail.com>
2024-12-12 23:55:58 +0300

    blogModel

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
-'use strict'
+"use strict";
+/* -------------------------------------------------------
+    EXPRESSJS - BLOG Project with Mongoose
+------------------------------------------------------- */
 
-/* ------------------------------------------------------- *
+const express = require("express");
+// Catch error from async:
+require("express-async-errors");
+const app = express(); //singleton
 
-EXPRESS: MONGOOSE
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
 
-/* ------------------------------------------------------- */
-
-const express=require('express')
-const app = express()
-
-require('dotenv').config()
-
-const PORT = process.env.PORT || 8000
-
-app.use(express.json())
-
-app.all('/', (req, res) => {
-    res.send('WELCOME TO BLOG APİ')
-})
-
-require('./src/dbConnection')
-
-app.use(require('./src/errorHandler'))
-
-
+// Accept JSON:
+app.use(express.json());
+// DB CONNECTION with normal function
+require("./src/config/dbConnection")();
+// DB CONNECTION with with class
+//require("./src/config/dbConnection");
 
 /* ------------------------------------------------------- */
 
+app.all("/", (req, res) => {
+  res.send("WELCOME TO BLOG API");
+});
 
+/* ------------------------------------------------------- */
+// Routes:
 
-app.listen(PORT, () => console.log('Running: http://127.0.0.1:' + PORT))
+/* ------------------------------------------------------- */
+app.use("*", (req, res) => {
+  res.status(404).send({ isError: true, message: "The route is NOT FOUND" });
+});
+
+// Catch Errors:
+app.use(require("./src/middlewares/errorHandler"));
+
+/* ------------------------------------------------------- */
+
+app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
