2b175ca261811826851e4e4234df24f5a3437ab3
cem <cemyilmaztech@gmail.com>
2024-12-30 23:15:05 +0300

    schema

--- src/models/blogModel.js
+++ src/models/blogModel.js
@@ -8,6 +8,10 @@ const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
 
 const blogSchema = new mongoose.Schema({
-    
+    name: {
+        type: String,
+        trim: true,
+        required: true
+    }
 })
 
